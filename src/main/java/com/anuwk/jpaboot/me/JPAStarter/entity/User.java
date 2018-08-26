028811063f1387a8b479ba215e70a2be25d899dd
Anuwk <un4ckn0wl3z@gmail.com>
2018-08-26 22:26:30 +0700

    h2

---
+++ src/main/java/com/anuwk/jpaboot/me/JPAStarter/entity/User.java
@@ -0,0 +1,45 @@
+package com.anuwk.jpaboot.me.JPAStarter.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	
+	private String name;
+	private String role;
+	
+	protected User() {
+		
+	}
+	
+
+	public User(String name, String role) {
+		this.name = name;
+		this.role = role;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("User [id=%s, name=%s, role=%s]", id, name, role);
+	}
+
+}
---
+++ src/main/java/com/anuwk/jpaboot/me/JPAStarter/service/UserDaoService.java
@@ -0,0 +1,23 @@
+package com.anuwk.jpaboot.me.JPAStarter.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.anuwk.jpaboot.me.JPAStarter.entity.User;
+
+@Repository
+@Transactional
+public class UserDaoService {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public long insert(User user) {
+		entityManager.persist(user);
+		return user.getId();
+	}
+
+}
